cac27a10344cfb9075c09a6f89bbb291359c557a
Max Kopych <max@cubicleninjas.com>
2018-11-21 18:35:45 -0600

    First settings

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  <h1 class="text-center my-5">Generate Your Team</h1>
+    <div v-if="!characters.length">
+      <h3 class="text-center">Loading...</h3>
+    </div>
+    <div v-else>
+      <div v-for="(character, index) in characters" class="col-xs-12 col-sm-6 col-md-4">
+        <Character :info="{character,index}"></Character>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Character from "./components/Character"
+export default {
+  name: 'app',
+  components:{
+    Character
+  },
+  data () {
+    return {
+      characters: []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Character.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card"></div>
+</template>
+
+<script>
+  export default {
+    name: "Character",
+    props: [
+      "info"
+    ],
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
