7bfabe2fc62bd6d17eef038aa4246912dd9f14b2
Max Kopych <maxkopych@gmail.com>
2018-11-21 20:13:09 -0600

    Playing with $root and $set

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
   <h1 class="text-center my-5">Generate Your Team</h1>
     <div v-if="!characters.length">
-      <h3 class="text-center">Loading...</h3>
+      <h3 class="text-center">Loading... {{total}}</h3>
     </div>
-    <div v-else>
+    <div v-else class="row">
       <div v-for="(character, index) in characters" class="col-xs-12 col-sm-6 col-md-4">
         <Character :info="{character,index}"></Character>
       </div>
@@ -21,7 +21,39 @@ export default {
   },
   data () {
     return {
-      characters: []
+      characters: [],
+      total: 0,
+    }
+  },
+  created(){
+    fetch("https://swapi.co/api/people/",{method:"GET"}).then( res=>res.json()).then( json => { //fetch сперва привотит все хеды а потом тело
+      this.total=json.count;
+      
+    })
+      .catch(error=>console.log(error));
+    
+    //########################################
+    this.$root.$on("changeSingleCharacter",(val)=>{
+      let $rand = Math.floor(Math.random() * this.total) + 1;
+      fetch("https://swapi.co/api/people/" + $rand ).then( res=>res.json()).then( json => {
+        json.index= $rand;
+        this.$set(this.characters,val,json)
+      })
+        .catch(error=>console.log(error));
+    })
+    //########################################
+  },
+  watch:{
+    total: function(val) { //Can't use Arrow Functions for watches
+      for(let i=0; i<3; i++){
+        let $rand = Math.floor(Math.random() * val) + 1;
+        fetch("https://swapi.co/api/people/" + $rand ).then( res=>res.json()).then( json => {
+          json.index= $rand
+          this.characters.push(json);
+          //
+        })
+          .catch(error=>console.log(error));
+      }
     }
   }
 }
--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="card"></div>
+  <div class="card" @click="changeCharacter">
+    <h3 class="card-title text-center text-warning">{{info.character.name}}</h3>
+    <ul class="list-group">
+      <li class="list-group-item">height: {{info.character.height}}</li>
+      <li class="list-group-item">mass: {{info.character.mass}}</li>
+      <li class="list-group-item">hair_color: {{info.character.hair_color}}</li>
+      <li class="list-group-item">skin_color: {{info.character.skin_color}}</li>
+      <li class="list-group-item">gender: {{info.character.gender}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -8,10 +17,17 @@
     props: [
       "info"
     ],
-    
+    methods:{
+      changeCharacter(){
+        this.$root.$emit("changeSingleCharacter",this.info.index);
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+.card{
+  cursor: pointer;
+  
+}
 </style>
